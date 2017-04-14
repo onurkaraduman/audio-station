eae7baabf5bb5908ad3237eb1bab04409c0602ad
Onur Karaduman <onrkrdmn@gmail.com>
2017-04-15 00:55:33 +0200

    coverall added

--- src/main/java/com/onrkrdmn/streamer/ConnectionPool.java
+++ src/main/java/com/onrkrdmn/streamer/ConnectionPool.java
@@ -1,20 +1,19 @@
 package com.onrkrdmn.streamer;
 
-import com.onrkrdmn.core.AudioBuffer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.onrkrdmn.converter.UrlConverter;
 import com.onrkrdmn.converter.YoutubeUrlConverter;
 import com.onrkrdmn.converter.model.Video;
+import com.onrkrdmn.core.AudioBuffer;
 import com.onrkrdmn.fetcher.VideoFetcher;
 import com.onrkrdmn.fetcher.YoutubeVideoFetcher;
+import com.onrkrdmn.streamer.constant.StreamerConst;
 import com.onrkrdmn.validator.UrlValidator;
 import com.onrkrdmn.validator.YoutubeUrlValidator;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Service;
-import com.onrkrdmn.streamer.constant.StreamerConst;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -30,10 +29,14 @@ import java.util.List;
 @Log4j
 public class ConnectionPool extends AbstractVerticle {
 
+    private ObjectMapper objectMapper;
+
     @PostConstruct
     public void init() {
         Vertx.vertx().deployVerticle(ConnectionPool.class.getName());
+        this.objectMapper = new ObjectMapper();
     }
+
 
     @Override
     public void start() throws Exception {
@@ -43,31 +46,28 @@ public class ConnectionPool extends AbstractVerticle {
             if (!ws.path().equals("/stream")) {
                 ws.reject();
             }
-            ws.handler(new Handler<Buffer>() {
-                @Override
-                public void handle(Buffer buffer) {
-                    String url = buffer.toString();
-                    log.info("Url>>>>>>>>>>" + url);
-                    UrlValidator validator = new YoutubeUrlValidator();
-                    validator.validate(url);
-                    UrlConverter converter = new YoutubeUrlConverter();
-                    List<Video> videos = null;
-                    try {
-                        videos = converter.convert(url);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    AudioBuffer videoBuffer = new AudioBuffer();
-                    VideoFetcher youtubeVideoFetcher = new YoutubeVideoFetcher(videos.get(0).getUrl(), videoBuffer);
-                    Streamer audioStreamer = new AudioStreamer(videoBuffer, ws);
-                    try {
-                        youtubeVideoFetcher.fetch();
-                        audioStreamer.stream();
-                    } catch (InterruptedException e) {
-                        log.error("Thread exepion.", e);
-                    } catch (IOException e) {
-                        log.error("I/O exception.", e);
-                    }
+            ws.handler(buffer -> {
+                String url = buffer.toString();
+                log.info("Url>>>>>>>>>>" + url);
+                UrlValidator validator = new YoutubeUrlValidator();
+                validator.validate(url);
+                UrlConverter converter = new YoutubeUrlConverter();
+                List<Video> videos = null;
+                try {
+                    videos = converter.convert(url);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                AudioBuffer videoBuffer = new AudioBuffer();
+                VideoFetcher youtubeVideoFetcher = new YoutubeVideoFetcher(videos.get(0).getUrl(), videoBuffer);
+                Streamer audioStreamer = new AudioStreamer(videoBuffer, ws);
+                try {
+                    youtubeVideoFetcher.fetch();
+                    audioStreamer.stream();
+                } catch (InterruptedException e) {
+                    log.error("Thread exepion.", e);
+                } catch (IOException e) {
+                    log.error("I/O exception.", e);
                 }
             });
         }).listen(StreamerConst.WEBSOCKET_PORT);
